0928aa405b0bf07915e155c10db156e7c9fbfdf4
Alexey Berdnikov <aberdnikov@gmail.com>
2016-07-21 21:30:48 +0500

    fixed

---
+++ init.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by Larakit.
+ * Link: http://github.com/larakit
+ * User: Alexey Berdnikov
+ * Date: 23.05.16
+ * Time: 10:02
+ */
+
+\Larakit\StaticFiles\Manager::package('larakit/sf-bootstrap-daterangepicker')
+    ->setSourceDir('public')
+    ->usePackage('larakit/sf-jquery')
+    ->usePackage('larakit/sf-momentjs')
+    ->cssPackage('daterangepicker.css')
+    ->jsPackage('daterangepicker.js');
